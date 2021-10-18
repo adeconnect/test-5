9b8ebd2ce8755353ccffae67b8033f96196d1c5e
Ade <ade@edconnect.ng>
2021-10-18 07:32:35 -0400

    Initial commit

---
+++ app/server/models/projects.js
@@ -0,0 +1,39 @@
+const DataModel = require('./data_model');
+
+class Project {
+    constructor(id, name, abstract, authors, tags, createdBy) {
+        this.id = id;
+        this.abstract = abstract;
+        this.authors = authors;
+        this.tags = tags;
+        this.createdBy = createdBy;
+    }
+}
+
+class Projects extends DataModel {
+    validate(obj) {
+        this.errors = [];
+
+        for (const requirements in obj) {
+            if(["authors", "tags"].includes(requirements)){
+                if(!Array.isArray(obj[requirements])){
+                    this.errors.push(`${requirements} should be an array`)
+                }
+            }else {
+            if(obj[requirements] === ""){
+                this.errors.push(`${requirements} should not be empty`)
+                }
+            }
+        }
+        
+        return this.errors.length > 0 ? false: true;
+    }
+
+
+}
+// Do not worry about the below for now; It is included so that we can test your code
+// We will cover module exports in later parts of this course
+module.exports = {
+    Project,
+    Projects
+};
---
+++ app/server/models/users.js
@@ -0,0 +1,79 @@
+const DataModel = require('./data_model');
+
+class User {
+    constructor(id, firstname, lastname, email, password, matricNumber, program, graduationYear) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.password = password;
+        this.matricNumber = matricNumber;
+        this.program = program;
+        this.graduationYear = graduationYear;
+    }
+
+    getFullName() {
+        const fullname = this.firstname + " " + this.lastname;
+        return fullname;
+    }
+}
+
+class Users extends DataModel {
+    authenticate(email, password) {// loop through this data model
+        for(const obj of this.data){
+            if (obj.email === email && obj.password === password){
+                return true;
+            }
+        }
+        return null;
+    }
+
+    getByEmail(email) {// loop through this data model
+        for(const obj of this.data){
+            if (obj.email === email){
+                return obj;
+            }
+        }
+        return null;
+    }
+
+    getByMatricNumber(matricNumber) {// loop through this data model
+        for (const obj of this.data){
+            if (obj.matricNumber === matricNumber){
+                return obj;
+            }
+        }
+        return null;
+    }
+
+    validate(obj) {// loop through this data model
+        this.errors  = [];
+
+        for (const requirements in obj){
+            if (obj[requirements] === ""){
+                this.errors.push(`${requirements} should not be empty`)
+            }
+        }
+
+        if (this.getByEmail(obj.email) === this.email){
+            this.errors.push(`A user with specified email address already exists`);
+        }
+
+        if (this.getByMatricNumber(obj.matricNumber) === this.matricNumber){
+            this.errors.push(`A user with specified matric number already exists`);
+        }
+
+        if (obj.password.length < 7){
+            this.errors.push(`Password should address already exists`);
+        }
+
+        return (this.errors.length === 0) ? true : false;
+    }
+}
+
+// Do not worry about the below for now; It is included so that we can test your code
+// We will cover module exports in later parts of this course
+module.exports = {
+    User,
+    Users
+};
